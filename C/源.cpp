16ea5e05570264d6cd56fdeb2a5cda8664c5f069
xiaoyustudio181 <35361157+xiaoyustudio181@users.noreply.github.com>
2018-03-10 22:36:39 +0800

    20180310.2236

---
+++ C/源.cpp
@@ -0,0 +1,22 @@
+/*Visual Studio 2013创建C语言项目的步骤：
+新建项目 -> Visual C++ -> Win32控制台应用程序 ->
+下一步 -> “应用程序类型”选择“控制台应用程序” -> 附加选项选择“空项目” ->
+右击项目文件夹中的“源文件” -> 添加 -> 新建项 -> C++文件(.cpp)
+最后直接在“源.cpp”文件编辑C语言程序。*/
+#include <stdio.h>
+int max(int a, int b)
+{
+	int m;
+	if (a > b)m = a;
+	else m = b;
+	return(m);
+}
+int main()
+{
+	printf("Hello world.\n");
+	int a = 1, b = 2;
+	printf("%d",a+b);
+	int c = max(a, b);
+	printf("%d", c);
+	getchar();
+}
